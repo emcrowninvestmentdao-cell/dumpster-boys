b1dcba3b6b670b720858b8ed06c53ffa605ab54e
EMC Crown Investment DAO <emcrowninvestmentdao@gmail.com>
2026-07-09 12:28:57 +0100

    Add $thebdizzle to the roster; sort holders by staking capacity
    Rows now sort low-to-high by max staking capacity for readability.

--- members.js
+++ members.js
@@ -3,4 +3,5 @@ const MEMBERS = [
   { name: 'Ollie',    wallet: '6459gxsejVJtsHtCtRsphr6bvdQQTQYh8Ucgse4w6KC' },
   { name: 'Redlight', wallet: '453RsqHmuZSiWZdHLXXwfYBzs4suCJyyC7RTCMhBBjDt' },
   { name: 'DevDad',   wallet: 'qg7pNNZq7qDQuc6Xkd1x4NvS2VM3aHtCqHEzucZxRGA' },
+  { name: '$thebdizzle', wallet: 'HEJu6wvJfARRGzoQ5VPhtq7uPB73fvnrDxqwiX3QPnPK' },
 ];
