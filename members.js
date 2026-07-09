78c80fc1e76531ce51e8c27369112bc5abedeec3
EMC Crown Investment DAO <emcrowninvestmentdao@gmail.com>
2026-07-09 13:57:20 +0100

    Add Humbled_Jay to the roster

--- members.js
+++ members.js
@@ -5,4 +5,5 @@ const MEMBERS = [
   { name: 'DevDad',   wallet: 'qg7pNNZq7qDQuc6Xkd1x4NvS2VM3aHtCqHEzucZxRGA' },
   { name: '$thebdizzle', wallet: 'HEJu6wvJfARRGzoQ5VPhtq7uPB73fvnrDxqwiX3QPnPK' },
   { name: 'pi3d pip3r', wallet: '3NKFbtbUt2QKD2De9ttG5cNcoUb6u3dLcfvRRcYW5NrL' },
+  { name: 'Humbled_Jay', wallet: 'HdJ1kvBgHf2vdXVrpyq32W8Mo6puQmX714noXsd6eAyr' },
 ];
