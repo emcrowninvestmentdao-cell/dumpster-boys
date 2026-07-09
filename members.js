8a3a1790f66eed5eb2ae59156c6576887a69e0c2
EMC Crown Investment DAO <emcrowninvestmentdao@gmail.com>
2026-07-09 15:00:38 +0100

    Add Micro7pigeons and VegasDan to the roster

--- members.js
+++ members.js
@@ -7,4 +7,6 @@ const MEMBERS = [
   { name: 'pi3d pip3r', wallet: '3NKFbtbUt2QKD2De9ttG5cNcoUb6u3dLcfvRRcYW5NrL' },
   { name: 'Humbled_Jay', wallet: 'HdJ1kvBgHf2vdXVrpyq32W8Mo6puQmX714noXsd6eAyr' },
   { name: 'Diz', wallet: 'B2k3W6GRSqxJvbmcniUDMsid9CygVnMNy9nJgmpdSkKg' },
+  { name: 'Micro7pigeons', wallet: '2kg4zLokSj36889VU5D1MUYZmB9T7MbHUyyVRY1VPPs5' },
+  { name: 'VegasDan', wallet: 'FgH3z4mA7wqHNVgSZgjjuJjxn2HRZwXoBW8uB7A3U22J' },
 ];
