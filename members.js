82662f8bd397c5b3037f0ffdeee9a2a394594c79
EMC Crown Investment DAO <emcrowninvestmentdao@gmail.com>
2026-07-09 12:05:25 +0100

    Add DevDad to the roster

--- members.js
+++ members.js
@@ -2,4 +2,5 @@
 const MEMBERS = [
   { name: 'Ollie',    wallet: '6459gxsejVJtsHtCtRsphr6bvdQQTQYh8Ucgse4w6KC' },
   { name: 'Redlight', wallet: '453RsqHmuZSiWZdHLXXwfYBzs4suCJyyC7RTCMhBBjDt' },
+  { name: 'DevDad',   wallet: 'qg7pNNZq7qDQuc6Xkd1x4NvS2VM3aHtCqHEzucZxRGA' },
 ];
